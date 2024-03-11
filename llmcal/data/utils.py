879fe5f6a8c861f5807db67ca0dc3b7d3f843788
LautaroEst <lautaro.est@gmail.com>
2024-03-11 08:57:49 -0300

    Redo load_from_hub

--- llmcal/data/utils.py
+++ llmcal/data/utils.py
@@ -5,7 +5,7 @@ from datasets import load_from_disk, Dataset
 import numpy as np
 from .datasets import SUPPORTED_DATASETS
 
-def _load_dataset(dataset_name: str) -> Dict[str,Dataset]:
+def load_dataset_from_hub(dataset_name: str) -> Dict[str,Dataset]:
     try:
         dataset_attrs = SUPPORTED_DATASETS[dataset_name]
     except KeyError:
@@ -15,15 +15,26 @@ def _load_dataset(dataset_name: str) -> Dict[str,Dataset]:
         full_dataset[split_name] = dataset_attrs['loading_function'](split_name)
     return full_dataset
 
+def load_dataset_from_disk(path: str) -> Dict[Dataset]:
+    full_dataset = {}
+    for split_name in ["train", "validation", "test"]:
+        full_dataset[split_name] = load_from_disk(os.path.join(path,split_name))
+    return full_dataset
+
 
 def load_dataset(
     dataset: str, 
+    load_from_hub: bool = False,
     random_state: int = 0,
     train_samples: Optional[int] = None,
     validation_samples: Optional[int] = None,
     test_samples: Optional[int] = None,
 )-> Dict[str,Dataset]:
-    splits = _load_dataset(dataset)
+    
+    if load_from_hub:
+        splits = load_dataset_from_hub(dataset)
+    else:
+        splits = load_dataset_from_disk(dataset)
 
     for n, split in zip([train_samples, validation_samples, test_samples],splits):
         data_split = splits[split]
