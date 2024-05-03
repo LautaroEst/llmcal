a612ddb7dcccb717d555661de00b2cf6f7df8fbc
LautaroEst <lautaro.est@gmail.com>
2024-05-03 19:02:08 -0300

    debug configs

--- run.01.lora.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python scripts/main.py \
-    --dataset sst2 \
-    --prompt prefix_basic_sst2 \
-    --data_fold all_zero_shot \
-    --model lm_tinyllama \
-    --method lora_r=8
---
+++ run.01.sst2.lm_tinyllama.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+MODEL=lm_tinyllama
+DATA_FOLD=n=10000_0-shot_6273
+
+python scripts/main.py \
+    --dataset sst2 \
+    --prompt prefix_basic_sst2 \
+    --data_fold $DATA_FOLD \
+    --model $MODEL \
+    --method no_adaptation
+
+python scripts/main.py \
+    --dataset sst2 \
+    --prompt prefix_basic_sst2 \
+    --data_fold $DATA_FOLD \
+    --model $MODEL \
+    --method affine_vector
+
+python scripts/main.py \
+    --dataset sst2 \
+    --prompt prefix_basic_sst2 \
+    --data_fold $DATA_FOLD \
+    --model $MODEL \
+    --method no_adaptation
