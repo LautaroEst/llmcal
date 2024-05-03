25f0416f6fed81a97cf478f0f4482eb66e06c818
LautaroEst <lautaro.est@gmail.com>
2024-05-03 18:30:13 -0300

    minor fix

--- llmcal/data/datasets/sst2.py
+++ llmcal/data/datasets/sst2.py
@@ -7,19 +7,8 @@ def load_sst2(data_dir):
     if not os.path.exists(data_dir):
         datadict = load_dataset("nyu-mll/glue", "sst2")
         rs = np.random.RandomState(7348)
-        idx = rs.permutation(len(datadict["train"]))[:100]
+        idx = rs.permutation(len(datadict["train"]))[:10000]
         datadict["train"] = datadict["train"].select(idx)
-
-
-        rs = np.random.RandomState(7348)
-        idx = rs.permutation(len(datadict["validation"]))[:100]
-        datadict["validation"] = datadict["validation"].select(idx)
-
-        rs = np.random.RandomState(7348)
-        idx = rs.permutation(len(datadict["test"]))[:100]
-        datadict["test"] = datadict["test"].select(idx)
-
-
         datadict.save_to_disk(data_dir)
     else:
         datadict = load_from_disk(data_dir)
