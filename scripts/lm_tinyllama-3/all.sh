f049ed03ffa6e1193347921619b54b96b946ce07
LautaroEst <lautaro.est@gmail.com>
2024-06-06 16:31:50 -0300

    Running banking77

--- scripts/lm_tinyllama-3/all.sh
+++ scripts/lm_tinyllama-3/all.sh
@@ -2,10 +2,10 @@
 echo "Starting TinyLlama"
 date
 
-bash scripts/lm_tinyllama-3/sst2.sh
+# bash scripts/lm_tinyllama-3/sst2.sh
 bash scripts/lm_tinyllama-3/agnews.sh
-bash scripts/lm_tinyllama-3/dbpedia.sh
-bash scripts/lm_tinyllama-3/20newsgroups.sh
+# bash scripts/lm_tinyllama-3/dbpedia.sh
+# bash scripts/lm_tinyllama-3/20newsgroups.sh
 bash scripts/lm_tinyllama-3/banking77.sh
 
 echo "TinyLlama complete"
