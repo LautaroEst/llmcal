b14e0e9322a7f05c822e883fd0af9ddc2886785c
LautaroEst <lautaro.est@gmail.com>
2024-08-30 18:18:20 +0000

    launching on llama3 again

---
+++ scripts/falta_correr_llama3.sh
@@ -0,0 +1,5 @@
+bash scripts/lm_llama3-1/all.sh
+bash scripts/lm_llama3-2/all.sh
+bash scripts/lm_llama3-3/all.sh
+bash scripts/lm_llama3-4/all.sh
+bash scripts/lm_llama3-5/all.sh
--- scripts/lm_llama3-1/all.sh
+++ scripts/lm_llama3-1/all.sh
@@ -2,11 +2,11 @@
 echo "Starting llama3"
 date
 
-# bash scripts/lm_llama3-1/sst2.sh
+bash scripts/lm_llama3-1/sst2.sh
 # bash scripts/lm_llama3-1/agnews.sh
-# bash scripts/lm_llama3-1/dbpedia.sh
-bash scripts/lm_llama3-1/20newsgroups.sh
-bash scripts/lm_llama3-1/banking77.sh
+bash scripts/lm_llama3-1/dbpedia.sh
+# bash scripts/lm_llama3-1/20newsgroups.sh
+# bash scripts/lm_llama3-1/banking77.sh
 
 echo "llama3 complete"
 date
