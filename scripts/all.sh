91f2d1a0bcb7f30f04ad197d558b52631142e297
LautaroEst <lautaro.est@gmail.com>
2024-09-03 14:20:20 +0000

    minor change

--- scripts/all.sh
+++ scripts/all.sh
@@ -4,14 +4,17 @@
 # bash scripts/lm_tinyllama-4/all.sh
 # bash scripts/lm_tinyllama-5/all.sh
 
-bash scripts/lm_phi3-1/all.sh
-bash scripts/lm_phi3-2/all.sh
-bash scripts/lm_phi3-3/all.sh
-bash scripts/lm_phi3-4/all.sh
-bash scripts/lm_phi3-5/all.sh
+# bash scripts/lm_phi3-1/all.sh
+# bash scripts/lm_phi3-2/all.sh
+# bash scripts/lm_phi3-3/all.sh
+# bash scripts/lm_phi3-4/all.sh
+# bash scripts/lm_phi3-5/all.sh
 
 # bash scripts/lm_llama3-1/all.sh
 # bash scripts/lm_llama3-2/all.sh
 # bash scripts/lm_llama3-3/all.sh
 # bash scripts/lm_llama3-4/all.sh
 # bash scripts/lm_llama3-5/all.sh
+
+bash scripts/falta_correr_phi3.sh
+bash scripts/falta_correr_llama3.sh
