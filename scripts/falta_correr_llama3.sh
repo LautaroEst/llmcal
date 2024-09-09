5e274968b43691d387368d63b498e715ab09952e
LautaroEst <lautaro.est@gmail.com>
2024-09-09 20:14:37 +0000

    minor change

--- scripts/falta_correr_llama3.sh
+++ scripts/falta_correr_llama3.sh
@@ -1,13 +1,7 @@
-python -m llmcal dbpedia_2_9722 basic_dbpedia_0-shot_litgpt lm_llama3 lora_20samples no_calibration --accelerator "gpu"
-
 python -m llmcal 20newsgroups_2_927 basic_20newsgroups_0-shot_litgpt lm_llama3 lora_40samples no_calibration --accelerator "gpu"
-python -m llmcal 20newsgroups_2_435 basic_20newsgroups_0-shot_litgpt lm_llama3 lora_40samples no_calibration --accelerator "gpu"
-python -m llmcal 20newsgroups_2_972 basic_20newsgroups_0-shot_litgpt lm_llama3 lora_40samples no_calibration --accelerator "gpu"
-python -m llmcal 20newsgroups_2_4351 basic_20newsgroups_0-shot_litgpt lm_llama3 lora_40samples no_calibration --accelerator "gpu"
-python -m llmcal 20newsgroups_2_9722 basic_20newsgroups_0-shot_litgpt lm_llama3 lora_40samples no_calibration --accelerator "gpu"
+python -m llmcal 20newsgroups_128_129 basic_20newsgroups_0-shot_litgpt lm_llama3 lora_1000samples no_calibration --accelerator "gpu"
+python -m llmcal 20newsgroups_128_131 basic_20newsgroups_0-shot_litgpt lm_llama3 lora_1000samples no_calibration --accelerator "gpu"
+python -m llmcal 20newsgroups_128_543 basic_20newsgroups_0-shot_litgpt lm_llama3 lora_1000samples no_calibration --accelerator "gpu"
+python -m llmcal 20newsgroups_128_878 basic_20newsgroups_0-shot_litgpt lm_llama3 lora_1000samples no_calibration --accelerator "gpu"
+python -m llmcal 20newsgroups_128_909 basic_20newsgroups_0-shot_litgpt lm_llama3 lora_1000samples no_calibration --accelerator "gpu"
 
-bash scripts/lm_llama3-1/20newsgroups.sh
-bash scripts/lm_llama3-2/20newsgroups.sh
-bash scripts/lm_llama3-3/20newsgroups.sh
-bash scripts/lm_llama3-4/20newsgroups.sh
-bash scripts/lm_llama3-5/20newsgroups.sh
